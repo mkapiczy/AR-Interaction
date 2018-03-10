12bded9f83a4195eb052c2fe6a76afb79d5026d9
Michal Kapiczynski <michal.kapiczynski@sirenia.eu>
2018-03-10 12:48:50 +0100

    Task C - face detection, circles around eyes, and unwarping it into the texture done

--- Assets/_4_Scripts/ScriptC.cs
+++ Assets/_4_Scripts/ScriptC.cs
@@ -5,59 +5,88 @@ using System.IO;
 
 public class ScriptC : MonoBehaviour {
 
-//    public Camera cam;
-//
-//	private GameObject plane;
-    
-//	// intrinsics
-//	public float fx = 650;
-//	public float fy = 650;
-//	public float cx = 320;
-//	public float cy = 240;
-//
-//	public Matrix4x4 originalProjection;
-//
-//	public int width = 640; 
-//	public int height = 480;
-//
-//    private MatOfPoint2f imagePoints;
-//    private Mat camImageMat;
-//	private VideoCapture cap(0);
-//
-//	Mat skullTextureMat;
-//
-//	Texture2D unwarpedTexture;
-//	Texture2D unwarpedTextureClean;
+	private VideoCapture videoCapture;
+	private GameObject faceTargetPlane;
+
+	private Mat camImageMat;
+	private Mat frontCameraImgMat;
+
+	private Mat faces;
+	private Mat eyes;
+	private Mat faceWithMarkings;
+
+	private string faceXML;
+	private string eyeXML;
+
+	private OpenCVForUnity.Rect roi;
+	private Texture2D unwarpedTexture;
+
+
+	public Camera cam;
+
+
 
     void Start () {
-//		plane = GameObject.Find ("Plane");
-//		unwarpedTextureClean = new Texture2D (width, height, TextureFormat.RGBA32, false);
+		faceTargetPlane = GameObject.Find ("FacePlane");
+		faceXML = "Assets/_2_Templates/haarcascade_frontalface_alt.xml";
+		eyeXML = "Assets/_2_Templates/haarcascade_eye.xml";
 
+		frontCameraImgMat = new Mat ();
+		faces = new Mat ();
+		eyes = new Mat ();
+		faceWithMarkings = new Mat ();
+
+		videoCapture = new VideoCapture ();
+		videoCapture.open (1);
     }
 
     void Update () {
+		Image camImg = CameraDevice.Instance.GetCameraImage(Image.PIXEL_FORMAT.RGBA8888);
 
-        //Access camera image provided by Vuforia
-//        Image camImg = CameraDevice.Instance.GetCameraImage(Image.PIXEL_FORMAT.RGBA8888);
-//		OpenCVForUnity.VideoCapture
-//        if(camImg!=null) {
-//            if (camImageMat == null) {
-//                //First time -> instantiate camera image specific data
-//                camImageMat = new Mat(camImg.Height, camImg.Width, CvType.CV_8UC4);  //Note: rows=height, cols=width
-//            }
-//
-//            camImageMat.put(0,0, camImg.Pixels);
-//
-//            //Replace with your own projection matrix. This approach only uses fy.
-//            cam.fieldOfView = 2 * Mathf.Atan(camImg.Height * 0.5f / fy) * Mathf.Rad2Deg;
-//
-//			unwarpedTexture = unwarpedTextureClean;
-//
-//			MatDisplay.MatToTexture(camImageMat, ref unwarpedTexture);
-//			plane.GetComponent<Renderer>().material.mainTexture = unwarpedTexture; 
-//
-//			MatDisplay.DisplayMat(camImageMat, MatDisplaySettings.BOTTOM_LEFT);
-////			MatDisplay.DisplayMat(mainImageMat, MatDisplaySettings.FULL_BACKGROUND);
-//        }
+		if (camImg != null) {
+			if (camImageMat == null) {
+				camImageMat = new Mat (camImg.Height, camImg.Width, CvType.CV_8UC4);  //Note: rows=height, cols=width
+			}
+			camImageMat.put(0, 0, camImg.Pixels);
+
+			// Read from videoCap and save in mat
+			videoCapture.read (frontCameraImgMat);
+
+			Face.getFacesHAAR (frontCameraImgMat, faces, faceXML);
+
+			Debug.Log("Faces " + faces.height ());
+			if (faces.height () > 0) {
+				for (var i = 0; i < faces.height (); i++) {
+					double[] faceRect = faces.get (i, 0);
+					Point faceRectPoint1 = new Point (faceRect [0], faceRect [1]);
+					Point faceRectPoint2 = new Point (faceRect [0] + faceRect [2], faceRect [1] + faceRect [3]);
+					Imgproc.rectangle (frontCameraImgMat, faceRectPoint1, faceRectPoint2, new Scalar (0, 0, 255), 5);
+					roi = new OpenCVForUnity.Rect (faceRectPoint1, faceRectPoint2);
+				}
+
+		 		faceWithMarkings = new Mat (frontCameraImgMat, roi);
+	
+				Face.getFacesHAAR (faceWithMarkings, eyes, eyeXML);
+
+				Debug.Log ("Eyes " + eyes.height ());
+				if (eyes.height () != 0) {
+					for (var i = 0; i < eyes.height(); i++) {
+						if (i < 2) {
+							double[] eyeRect = eyes.get (i, 0);
+							Point eyeCenter = new Point (eyeRect [2] * 0.5F + eyeRect [0], eyeRect [3] * 0.5F + eyeRect [1]);
+							int radius = (int)Mathf.Sqrt (Mathf.Pow (((float)eyeRect [2]) * 0.5F, 2F) + Mathf.Pow (((float)eyeRect [3]) * 0.5F, 2F));
+							Imgproc.circle (faceWithMarkings, new Point (eyeCenter.x, eyeCenter.y), radius, new Scalar (255, 0, 0), 5);
+						}
+					}
+				} 
+
+				MatDisplay.MatToTexture (faceWithMarkings, ref unwarpedTexture);
+				faceTargetPlane.GetComponent<Renderer> ().material.mainTexture = unwarpedTexture; 
+			}
+		}
+			
+		MatDisplay.DisplayMat(camImageMat, MatDisplaySettings.FULL_BACKGROUND);
+		MatDisplay.DisplayMat (frontCameraImgMat, MatDisplaySettings.BOTTOM_LEFT);
+
     }
 }
